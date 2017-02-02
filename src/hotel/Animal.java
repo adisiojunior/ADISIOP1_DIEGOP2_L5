b299456758b7aea3a77ed4e44fb22c2c22c3f100
Diego Aquino <diego.aquino@ccc.ufcg.edu.br>
2017-02-02 17:26:55 -0300

    Editei a classe Animal

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -1,5 +1,50 @@
 package hotel;
 
 public class Animal {
+	private String nome;
+	private String tipo;
+	private String idade;
+	
+	public Animal(String nome, String tipo, String idade){
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Animal){
+			Animal papagaio = (Animal) obj;
+			if(papagaio.getNome().equalsIgnoreCase(this.getNome()) && papagaio.getTipo().equalsIgnoreCase(getTipo()))
+			return true;
+			}
+		return false;
+	}
+	
+	
 }
